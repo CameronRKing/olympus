1ec111366f6dc67e28b13bbd2b28a2e1453f1ecc
Cameron King <cameronking42@gmail.com>
2020-01-20 15:32:20 -0600

    Renames Vue attributes in text templates.

--- src/RenameAttrInHtml.js
+++ src/RenameAttrInHtml.js
@@ -8,20 +8,50 @@ const { pairs, mapWithKeys } = require('./utils');
  * @param {String} newName 
  */
 function renameAttrInHtml(hast, name, newName) {
+   renameInHtmlAttributes(hast, name, newName);
+   renameInTextTemplates(hast, name, newName);
+}
+
+function renameInHtmlAttributes(hast, name, newName) {
     hast.match({ tag: /.*/ }, node => {
-        if (!(node && node.attrs)) return node;
+        if (!node.attrs) return node;
         const vueAttrs = pairs(node.attrs).filter(([key, val]) => key.includes(':') || key.includes('@') || key.includes('v-'));
         const parsedAttrs = vueAttrs.map(([key, val]) => [key, j(val)]);
         const renamedAttrs = parsedAttrs.map(([key, jsrc]) => {
-            jsrc.find(j.Identifier, { name }).map(path => {
-                path.replace(j.identifier(newName));
-                return path;
-            });
+            replaceIdentifiers(jsrc, name, newName);
             return [key, jsrc];
         });
         renamedAttrs.forEach(([key, jsrc]) => {
             console.log(node);
             node.attrs[key] = jsrc.toSource({ quote: 'single' })
+        });
+        return node;
+    });
+}
+
+function replaceIdentifiers(jsrc, name, newName) {
+    jsrc.find(j.Identifier, { name }).map(path => {
+        path.replace(j.identifier(newName));
+        return path;
+    });
+}
+
+function renameInTextTemplates(hast, name, newName) {
+    hast.match({ tag: /.*/ }, node => {
+        if (!(node.content && node.content.length > 0)) return node;
+        node.content = node.content.map(child => {
+            if (typeof child !== 'string') return child;
+            if (!child.match('{{([^}]+)}}')) return child;
+            // I need to find, parse, update, and replace the template innards
+            // assuming that the HTML doesn't naturally contain {{
+            const statements = child.split('{{');
+            const updated = statements.slice(1).map(statement => {
+                const [expression, trailing] = statement.split('}}');
+                const jsrc = j(expression);
+                replaceIdentifiers(jsrc, name, newName);
+                return '{{' + jsrc.toSource({ quote: 'single' }) + '}}' + trailing;
+            });
+            return statements[0] + updated.join('');
         });
         return node;
     });
--- test/unit/RenameAttrInHtml.spec.js
+++ test/unit/RenameAttrInHtml.spec.js
@@ -6,5 +6,11 @@ describe('RenameAttrInHtml', () => {
         const res = await posthtml().process('<div class="foo" :class="foo" @click="console.log(foo)" v-for="item in foo"></div>');
         renameAttrInHtml(res.tree, 'foo', 'bar');
         expect(res.html).to.equal('<div class="foo" :class="bar" @click="console.log(bar)" v-for="item in bar"></div>');
-    })
+    });
+
+    it('renames the identifier in text templates', async () => {
+        const res = await posthtml().process('<div>{{ foo }} {{ JSON.stringify(foo) }}</div>');
+        renameAttrInHtml(res.tree, 'foo', 'bar');
+        expect(res.html).to.equal('<div>{{ bar }} {{ JSON.stringify(bar) }}</div>');
+    });
 });
